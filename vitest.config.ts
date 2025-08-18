d7125ecbddb346fbe26fc30467e35a5bf21e3ed4
Vincent Dubois <vdubois@coreoz.com>
2025-08-18 17:35:09 +0200

    Precise source directory for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config.js';
 
 export default mergeConfig(viteConfig, defineConfig({
   test: {
+    dir: 'src',
     environment: "node",
     globals: true, // Ensure globals like 'expect' are available
   },
